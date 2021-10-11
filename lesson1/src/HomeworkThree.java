1649d0cc8a39757a273f319b99f74bb65ee157a9
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2021-10-11 15:26:22 +0300

    Done fix homework 15:23 11.10.21 Сделал исправления согласно комментариям. Исправил неточности в названиях, везде, где можно сократил запись до i < a.length, у compareSumOfPieces появился универсальный(вроде) метод, ровно как и обоих методов из 8ой задачи

--- lesson1/src/HomeworkThree.java
+++ lesson1/src/HomeworkThree.java
@@ -14,58 +14,71 @@ public class HomeworkThree {
         int[] fifthArray = new int[] {1, 4, 6, 9, 2}; // #6
         searchForMinMax(fifthArray);
           int[] sixthArray = new int[] {4, 5, 7, 1, 2};
-          System.out.println(compareSumOfPieces(sixthArray));
+          System.out.println(compareSumOfPieces(sixthArray)); // #7
           int[] seventhArrayV1 = new int[] {1, 2, 3, 4};
           int[] seventhArrayV2 = new int[] {1, 2, 3, 4};
-          int n = 3;
-          int m = -3;
-        ClockwiseArray(seventhArrayV1, n);
-        Counterclockwise(seventhArrayV2, m);
+          int n = 1;
+          int m = -1;
+        clockwiseArrayV2(seventhArrayV1, n); // #8
+        counterclockwiseV2(seventhArrayV2, m); // #8
 
         System.out.println(Arrays.toString(seventhArrayV1));
         System.out.println(Arrays.toString(seventhArrayV2));
 
     }
 
-    private static void Counterclockwise(int[] seventhArray, int m) {
-        for (int i = -1; i >= m; i--) {
-            int a = seventhArray[0];
-            int b = seventhArray[1];
-            int c = seventhArray[2];
-            int d = seventhArray[3];
-            seventhArray[0] = b;
-            seventhArray[1] = c;
-            seventhArray[2] = d;
-            seventhArray[3] = a;
+
+    private static void clockwiseArrayV2(int[] seventhArray, int n) {
+        for (int i = 1; i <= n; i++) {
+            int first = seventhArray[0];
+            for (int j = seventhArray.length - 1; j > 0; j--) {
+                if (seventhArray[j] < seventhArray.length){
+                    seventhArray[j + 1] = seventhArray[j];
+                } else {
+                    seventhArray[0] = seventhArray[j];
+                }
+            }
+            seventhArray[1] = first;
+
         }
     }
 
-    private static void ClockwiseArray(int[] seventhArray, int n) {
-        for (int i = 1; i <= n; i++) {
-          int a = seventhArray[0];
-          int b = seventhArray[1];
-          int c = seventhArray[2];
-          int d = seventhArray[3];
-          seventhArray[0] = d;
-          seventhArray[1] = a;
-          seventhArray[2] = b;
-          seventhArray[3] = c;
+    private static void counterclockwiseV2(int[] seventhArray, int m) {
+        for (int i = -1; i >= m; i--) {
+            int first = seventhArray[0];
+            for (int j = 0; j < seventhArray.length - 1; j++) {
+                if (seventhArray[j + 1] < seventhArray.length){
+                    seventhArray[j] = seventhArray[j + 1];
+                } else {
+                    seventhArray[j] = seventhArray[seventhArray.length - 1];
+                }
+            }
+            seventhArray[seventhArray.length - 1] = first;
+
         }
     }
 
     private static boolean compareSumOfPieces(int[] sixthArray) {
-        return sixthArray[0] + sixthArray[1] == sixthArray[2] + sixthArray[3] + sixthArray[4];
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < sixthArray.length / 2; i++) {
+            a += sixthArray[i];
+        }
+        for (int i = sixthArray.length / 2; i >= sixthArray.length / 2 && i < sixthArray.length; i++) {
+            b += sixthArray[i];
+        }
+        return a == b;
     }
 
     private static void searchForMinMax(int[] fifthArray) {
         int min = fifthArray[0];
         int max = fifthArray[0];
-        for (int i = 0; i <= fifthArray.length - 1; i++) {
+        for (int i = 1; i < fifthArray.length; i++) {
             if (max < fifthArray[i]){
                 max = fifthArray[i];
             }
         }
-        for (int i = 0; i <= fifthArray.length - 1; i++) {
+        for (int i = 1; i < fifthArray.length; i++) {
             if (min > fifthArray[i]){
                 min = fifthArray[i];
             }
@@ -86,7 +99,9 @@ public class HomeworkThree {
             for (int j = 0; j <= fourthArray[i].length - 1; j++) {
                 if (i == j){
                     fourthArray[i][j] = 1;
-                } else fourthArray[i][j] = 0;
+                } else {
+                    fourthArray[i][j] = 0;
+                }
             }
         }
         for (int i = fourthArray.length - 1; i >= 0 ; i--) {
@@ -105,7 +120,7 @@ public class HomeworkThree {
     }
 
     private static void lowNumberMultiplication(int[] thirdArray) {
-        for (int i = 0; i < thirdArray.length - 1; i++) {
+        for (int i = 0; i < thirdArray.length; i++) {
            if (thirdArray[i] < 6){
                thirdArray[i] = thirdArray[i] * 2;
            }
@@ -114,14 +129,14 @@ public class HomeworkThree {
     }
 
     private static void sequentialFilling(int[] secondArray) {
-        for (int i = 0; i <= secondArray.length - 1; i++) {
+        for (int i = 0; i < secondArray.length; i++) {
             secondArray[i] = i + 1;
         }
         System.out.println(Arrays.toString(secondArray));
     }
 
     private static void changeNumbersOfArray(int[] firstArray) {
-        for (int i = 0; i <= firstArray.length - 1; i++) {
+        for (int i = 0; i < firstArray.length; i++) {
             if (firstArray[i] == 1){
                 firstArray[i] = 0;
             } else {
