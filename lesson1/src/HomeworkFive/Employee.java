fb397382974fe0bba452ce3fd70c98460c429d5f
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2021-10-17 21:58:58 +0300

    Попытка нормального слияния веток(из-за нехватки опыта работы с Git и непроверенных домашних работ в другой ветке возникла путаница как что добавить)

---
+++ lesson1/src/HomeworkFive/Employee.java
@@ -0,0 +1,84 @@
+package HomeworkFive;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String employeeEmail;
+    private String number;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String employeeEmail,
+                    String number, int salary, int age){
+        this.fullName = fullName;
+        this.position = position;
+        this.employeeEmail = employeeEmail;
+        this.number = number;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void EmployeeStatus(){
+        System.out.println("Запрошенная карта сотрудника");
+        System.out.println("ФИО сотрудника: " + getFullName());
+        System.out.println("Позиция в компании: " + getPosition());
+        System.out.println("Email для связи: " + getEmployeeEmail());
+        System.out.println("Номер телефона для связи: " + getNumber());
+        System.out.println("Заработная плата: " + getSalary());
+        System.out.println("Возраст: " + getAge());
+
+    }
+
+    //    Все get-методы ---------------------------------------------------
+    public int getAge() {
+        return age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    //    Все set-методы ---------------------------------------------------
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+//    -------------------------------------------------------------------
+}
+
+
+
---
+++ lesson1/src/HomeworkFive/HomeworkFive.java
@@ -0,0 +1,34 @@
+package HomeworkFive;
+
+
+
+public class HomeworkFive {
+
+    public static void main(String[] args) {
+
+        Employee[] companyEmployees = new Employee[5];
+        companyEmployees[0] = new Employee("Бузас Витовтас Дарио", "Учащийся",
+                "vitovtasbuzas@gmail.com",  "89110278421", -6000, 23);
+        companyEmployees[1] = new Employee("Иванов Иван Иванович","Человек-заглушка","ivan@list.ru",
+                "89151234567",15000,31);
+        companyEmployees[2] = new Employee("Невероятно Cтарый Cотрудник","Человек предпенсионного возраста",
+                "diplodocus@yandex.ru","89001234567",14500,255);
+        companyEmployees[3] = new Employee("Друзь Александр Абрамович","Магистр Что?Где?Когда?",
+                "WhatWhereWhen@mail.ru","88005553535",250000,66);
+        companyEmployees[4] = new Employee("Ульяненко Максим Анатольевич","Пограничник(по возрасту)","ulamax@gmail.com",
+                "89899888989",25000,40);
+
+        System.out.println("Вот запрошенные результаты: сотрудники старше 40 лет");
+        for (int i = 0; i < companyEmployees.length; i++) {
+            if (companyEmployees[i].getAge() > 40){
+                companyEmployees[i].EmployeeStatus();
+            }
+        }
+
+
+
+
+
+
+    }
+}
